054c49135ddcb4882d3c00c7a7ffd0e9a13afbec
Matteo Senardi <pualien@gmail.com>
2023-08-21 11:45:16 +0200

    updated pyYAML

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def get_requirements(*requirements_file):
 
 
 setup(name='pymongo-ssh',
-      version='0.0.13',
+      version='0.0.14',
       url='https://github.com/pualien/pymongo-ssh',
       license='MIT',
       author='Matteo Senardi',
